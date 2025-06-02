5665d1cf7d48a6c80d4ea2960e43c9785b92bffe
elena5100 <194572363+elena5100@users.noreply.github.com>
2025-06-02 03:33:49 -0700

    Add tests for explorer in blocked spots, edges, and full island

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -62,4 +62,72 @@ public class ExplorerSearchTest {
         assertEquals(4, actual);
     }
 
+    
+
+    @Test
+    public void testSurroundedByWalls() {
+        int[][] island = {
+            {1,3,1},
+            {3,0,3},
+            {1,3,1}
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void testAllMountains() {
+        int[][] island = {
+            {3,3,3},
+            {3,0,3},
+            {3,3,3}
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void testAllFields() {
+        int[][] island = {
+            {0,1,1,1},
+            {1,1,1,1},
+            {1,1,1,1}
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(12, actual);
+    }
+
+    @Test
+public void testExplorerOnEdge() {
+    int[][] island = {
+        {0,1,2},
+        {3,1,2},
+        {2,1,1}
+    };
+    int actual = ExplorerSearch.reachableArea(island);
+    assertEquals(5, actual); 
+}
+
+
+    @Test
+    public void testDisconnectedLand() {
+        int[][] island = {
+            {0,2,1},
+            {2,2,2},
+            {1,1,1}
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void testMultiplePathsSameResult() {
+        int[][] island = {
+            {0,1,1},
+            {1,1,1},
+            {1,1,1}
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(9, actual);
+    }
 }
